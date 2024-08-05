cc7c9afb8c35667202e3932516a662c6b71d6715
obynonwane <biostechnologyng@gmail.com>
2024-08-05 18:06:43 +0100

    Number Input element

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,15 +14,27 @@ export default StartGameScreen;
 
 styles = StyleSheet.create({
   inputContainer: {
+    alignItems: "center",
     marginTop: 100, // push the view screen away from the top of the device
     marginHorizontal: 24, //give space on the left & right
     padding: 16,
     backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4, //background shadow on android
-    shadowColor: "black", //shadow collor for ios
+    shadowColor: "black", //shadow color for ios
     shadowOffSet: { width: 0, height: 2 }, //ios
     shadowRadius: 6, // ios
     shadowOpacity: 0.25, // ios how transparent
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
