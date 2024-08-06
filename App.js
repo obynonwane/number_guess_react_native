64ec201fddd6d58935fa1531ccc050d9b8f241e2
obynonwane <biostechnologyng@gmail.com>
2024-08-06 16:42:43 +0100

    Creating a Title Component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import { StyleSheet, View, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -24,7 +24,7 @@ export default function App() {
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
