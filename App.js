43f574914a0d7255072716979c0ee9ddd7e80c17
obynonwane <biostechnologyng@gmail.com>
2024-08-06 02:05:01 +0100

    Adding a Linear Gradient

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <LinearGradient
+        colors={["#4e0329", "#ddb52f"]} // Specify your gradient colors here
+        style={styles.gradient}>
+        <StartGameScreen />
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  gradient: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     marginTop: 100, // push the view screen away from the top of the device
     marginHorizontal: 24, //give space on the left & right
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4, //background shadow on android
     shadowColor: "black", //shadow color for ios
